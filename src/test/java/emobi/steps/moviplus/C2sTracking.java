819ca51a2a9568f0aa82c17443fbf3659b901f24
thuannguyen379 <thuan@emobi.biz>
2022-08-09 16:46:00 +0700

    add tcs apis Moviplus Msisdn-mo

--- src/test/java/emobi/steps/moviplus/C2sTracking.java
+++ src/test/java/emobi/steps/moviplus/C2sTracking.java
@@ -25,8 +25,8 @@ public class C2sTracking {
         config = new ConfigSettings(System.getProperty("user.dir"));
     }
 
-    @Given("User call the API tracking request of Mobwizard flow C2S with valid credential")
-    public void user_call_the_api_tracking_request_of_mobwizard_flow_c2s_with_valid_credential(List<List<String>> list) {
+    @Given("User call the API tracking request of Moviplus flow C2S with valid credential")
+    public void user_call_the_api_tracking_request_of_moviplus_flow_c2s_with_valid_credential(List<List<String>> list) {
         result = Utils.generateListString(list);
         restRequest = new RestRequest(URL.BASE_URL_HAS_PORT_8091, URL.AFFILIATES_PARTNER_C2S + result.get(3), RestMethod.GET);
         restHeaders.add("apikey", config.getApiKey());
@@ -38,13 +38,13 @@ public class C2sTracking {
         restResponse.printPrettyPrint();
     }
 
-    @Then("Response successful status code is {int} after tracking request of Mobwizard flow C2S")
-    public void response_successful_status_code_is_after_tracking_request_of_mobwizard_flow_c2s(int statusCode) {
+    @Then("Response successful status code is {int} after tracking request of Moviplus flow C2S")
+    public void response_successful_status_code_is_after_tracking_request_of_moviplus_flow_c2s(int statusCode) {
         Assert.assertEquals(statusCode, restResponse.extract().statusCode());
     }
 
-    @Then("Response successful message status keyword shortcode and trackingcode not null after tracking request of Mobwizard flow C2S")
-    public void response_successful_message_status_keyword_shortcode_and_trackingcode_not_null_after_tracking_request_of_mobwizard_flow_c2s(List<List<String>> list) {
+    @Then("Response successful message status keyword shortcode and trackingcode not null after tracking request of Moviplus flow C2S")
+    public void response_successful_message_status_keyword_shortcode_and_trackingcode_not_null_after_tracking_request_of_moviplus_flow_c2s(List<List<String>> list) {
         result = Utils.generateListString(list);
         Assert.assertEquals(result.get(4), restResponse.getJsonPath(restResponse.extract()).getString("message"));
         Assert.assertEquals(result.get(5), restResponse.getJsonPath(restResponse.extract()).getString("status"));
